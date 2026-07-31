a57b138a16f2b397e722623a9572b00308a40b47
Anshu Ingle <anshu.ingle2105@gmail.com>
2026-07-31 23:54:05 +0530

    Configured app to use environment variables

--- app.py
+++ app.py
@@ -37,10 +37,11 @@ app.config['SESSION_START_TOKEN'] = str(uuid.uuid4())
 # secret key
 app.secret_key = 'suraj'
 # Mysql Connection
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'career_roadmap_db'
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(app)
 
